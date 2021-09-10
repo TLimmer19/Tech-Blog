7301afc5278e2f6a4673c3c40b265863811cdb89
TLimmer19 <tlimmer19@me.com>
2021-09-10 18:36:07 -0500

    added dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,74 +15,45 @@ router.get('/', async (req, res) => {
   })
 })
 
-// GET all galleries for homepage
 // router.get('/', async (req, res) => {
+//   Post.findByPk(req.params.id,{
+//     include: [Comment]
+//   }).then(data => {
+//     console.log(data)
+//     const hbsObj = {
+//       postArr: data
+//     }
+//     res.render('home', hbsObj);
+//   })
+// })
+// router.post("/api/users/addpost", async (req, res) => {
+ 
 //   try {
-//     const dbGalleryData = await Gallery.findAll({
-//       include: [
-//         {
-//           model: Painting,
-//           attributes: ['filename', 'description'],
-//         },
-//       ],
+//     const postData = await Posts.create({
+//       user: req.body.user,
+//       text: req.body.text,
+//       posting_date: req.body.posting_date
 //     });
+   
+//     res.status(200).json(postData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+// router.get("/Comments/:id", withAuth, async (req, res) => {
+ 
+//   try {
+//     const dbCommentsData = await Comments.findByPk(req.params.id);
 
-//     const galleries = dbGalleryData.map((gallery) =>
-//       gallery.get({ plain: true })
-//     );
+//     const Comments = dbCommentsData.get({ plain: true });
 
-//     res.render('homepage', {
-//       galleries,
-//       loggedIn: req.session.loggedIn,
-//     });
+//     res.render("comments", { Comments, loggedIn: req.session.loggedIn });
 //   } catch (err) {
 //     console.log(err);
 //     res.status(500).json(err);
 //   }
 // });
 
-// // GET one gallery
-// // Use the custom middleware before allowing the user to access the gallery
-// router.get('/gallery/:id', withAuth, async (req, res) => {
-//   try {
-//     const dbGalleryData = await Gallery.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: Painting,
-//           attributes: [
-//             'id',
-//             'title',
-//             'artist',
-//             'exhibition_date',
-//             'filename',
-//             'description',
-//           ],
-//         },
-//       ],
-//     });
-
-//     const gallery = dbGalleryData.get({ plain: true });
-//     res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-// // GET one painting
-// // Use the custom middleware before allowing the user to access the painting
-// router.get('/painting/:id', withAuth, async (req, res) => {
-//   try {
-//     const dbPaintingData = await Painting.findByPk(req.params.id);
-
-//     const painting = dbPaintingData.get({ plain: true });
-
-//     res.render('painting', { painting, loggedIn: req.session.loggedIn });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).json(err);
-//   }
-// });
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
@@ -90,7 +61,20 @@ router.get('/login', (req, res) => {
     return;
   }
 
-  res.render('login');
+  res.render('login',);
 });
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup',);
+});
+
+// router.get('/newpost', (req, res) => {
+//   res.render('newpost');
+// });
+
 
 module.exports = router;
